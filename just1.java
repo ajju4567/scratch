86bbb81285ff313ef052c9b8e3e41ad59a2e8adb
ajju4567 <40421422+ajju4567@users.noreply.github.com>
2018-06-28 14:32:03 +0530

    Update just1.java

--- just1.java
+++ just1.java
@@ -2,6 +2,9 @@ public class test{
 	public static void main(String[] args){
 	System.out.println("Hi, Welcome to GIT");
 	System.out.println("Raja");
+		for(i=0;i<10;i++){
+		System.out.println("Number"+i);
+		}
 } 
 }
 
